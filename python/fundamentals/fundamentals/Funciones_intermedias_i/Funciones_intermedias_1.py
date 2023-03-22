dad671481fdf467d0927a08107008a2e9f766fab
may cryy <mauricio.munoz.gonzalez@alumnos.sip.cl>
2023-03-22 17:55:58 +0100

    zsda

---
+++ python/fundamentals/fundamentals/Funciones_intermedias_i/Funciones_intermedias_1.py
@@ -0,0 +1,28 @@
+x = [ [5,2,3], [10,8,9] ] 
+estudiantes = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+
+]
+directorio_deportes = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'fútbol' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+#3
+
+
+estudiantes = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iteratedictionary(estudiantes, name):
+    for i in range(len(estudiantes) - len(estudiantes), len(estudiantes)):
+        for key, value in estudiantes[name].items():
+            print(value)
+            return
+    
+print(iteratedictionary(estudiantes))
