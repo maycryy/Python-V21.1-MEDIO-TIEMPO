11eca6ee17a4f684b8e7bdffd7b8b50520b4c5ea
may cryy <mauricio.munoz.gonzalez@alumnos.sip.cl>
2023-03-27 20:28:16 +0200

    dfaf

---
+++ python/fundamentals/fundamentals/oop/Usuarios.py
@@ -0,0 +1,45 @@
+class Usuario:
+    def __init__(self, name, email_address, balance) -> None:
+            self.name = name
+            self.email = email_address
+            self.balance_mount = balance
+    def hacer_deposito(self, amount):
+        self.balance_mount += amount
+    def hacer_retiro(self, other_user):
+        self.balance_mount -= other_user
+    def hacer_transferencia(self, amount):
+        self.balance_mount += amount
+        self.balance_mount -= amount
+        self.imprime()
+        self.imprime()
+
+name ='pirata'
+email='pirata@gmail.com'
+balance = 1200
+pirata= Usuario(name, email, balance)
+
+name ='happy'
+email='happy@gmail.com'
+balance = 305
+happy= Usuario(name, email, balance)
+
+name ='rocky'
+email='rocky@gmail.com'
+balance = -7500
+rocky= Usuario(name, email, balance)
+print(f" user:{happy.name} , balance:{happy.balance_mount}")
+print(f" user:{pirata.name} , balance:{pirata.balance_mount}")
+print(f" user:{rocky.name} , balance:{rocky.balance_mount}")
+
+
+
+happy.hacer_deposito(300)
+happy.hacer_deposito(300)
+happy.hacer_deposito(300)
+happy.hacer_retiro(900)
+
+pirata.hacer_transferencia(1000) 
+
+
+print(f" user:{happy.name} , balance:{happy.balance_mount}")
+print(f" user:{pirata.name} , balance:{pirata.balance_mount}")
